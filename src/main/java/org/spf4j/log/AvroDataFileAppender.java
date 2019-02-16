c4b9e9c58702fd1016628dae5f20cc8806d199d1
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-02-16 14:21:03 -0500

    [add] a avro data file appender.

---
+++ src/main/java/org/spf4j/log/AvroDataFileAppender.java
@@ -0,0 +1,174 @@
+/*
+ * Copyright 2019 SPF4J.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spf4j.log;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.UnsynchronizedAppenderBase;
+import java.io.File;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.concurrent.TimeUnit;
+import org.apache.avro.file.CodecFactory;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.specific.SpecificDatumWriter;
+import org.spf4j.base.avro.LogRecord;
+import org.spf4j.concurrent.FileBasedLock;
+import org.spf4j.jmx.JmxExport;
+import org.spf4j.jmx.Registry;
+
+/**
+ * an Appender that will log into binary avro data files.
+ * Date will be partition by date
+ *
+ * @author Zoltan Farkas
+ */
+public final class AvroDataFileAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
+
+  private static final ZoneId ZULU = ZoneId.of("Z");
+
+  private String fileNameBase;
+
+  private DataFileWriter<LogRecord> writer;
+
+  private LocalDate fileDate;
+
+  private File currentFile;
+
+  private File destinationPath;
+
+  private ZoneId zoneId;
+
+  private CodecFactory codecFact;
+
+  public AvroDataFileAppender() {
+    zoneId = ZULU;
+    fileNameBase = ManagementFactory.getRuntimeMXBean().getName();
+    setName("avroLogAppender");
+
+    try {
+      Class.forName("org.xerial.snappy.Snappy");
+      codecFact = CodecFactory.snappyCodec();
+    } catch (ClassNotFoundException ex) {
+     codecFact = null;
+    }
+  }
+
+  public void setFileNameBase(final String fileNameBase) {
+    this.fileNameBase = fileNameBase;
+  }
+
+  public void setDestinationPath(final File destinationPath) {
+    this.destinationPath = destinationPath;
+  }
+
+  public void setPartitionZoneID(final String zoneId) {
+    this.zoneId = ZoneId.of(zoneId);
+  }
+
+  @JmxExport
+  public File getCurrentFile() {
+    return currentFile;
+  }
+
+  @Override
+  public void stop() {
+    try {
+      writer.close();
+      super.stop();
+    } catch (IOException | RuntimeException ex) {
+      addError("Unable to close writer " + writer, ex);
+    }
+    Registry.unregister("avro.log.appender", this.getName());
+  }
+
+  @Override
+  public void start() {
+    Instant now = Instant.now();
+    try {
+      ensurePartition(now);
+      super.start();
+    } catch (IOException | InterruptedException | RuntimeException ex) {
+       addError("Unable to ensure file partition " + now, ex);
+    }
+    Registry.export("avro.log.appender", this.getName(), this);
+  }
+
+  private void ensurePartition(final Instant instant) throws IOException, InterruptedException {
+    ZonedDateTime zdt = instant.atZone(zoneId);
+    LocalDate target = zdt.toLocalDate();
+    if (target.equals(fileDate)) {
+      return;
+    } else {
+      if (writer != null) {
+        writer.close();
+      }
+      fileDate = target;
+      writer = new DataFileWriter<>(new SpecificDatumWriter<>(LogRecord.class));
+      if (codecFact != null) {
+        writer.setCodec(codecFact);
+      }
+      String fileName = fileNameBase + '_' + target.toString() + ".avro";
+      currentFile = new File(destinationPath, fileName);
+      FileBasedLock lock = FileBasedLock.getLock(new File(destinationPath, fileName + ".lock"));
+      boolean locked = lock.tryLock(1, TimeUnit.MINUTES);
+      if (locked) {
+        try {
+          if (currentFile.canWrite()) {
+            writer = writer.appendTo(currentFile);
+          } else {
+            writer.create(LogRecord.getClassSchema(), currentFile);
+          }
+        } finally {
+          lock.unlock();
+        }
+      } else {
+        throw new IOException("cannot acquire lock " + lock);
+      }
+    }
+  }
+
+  @Override
+  protected void append(final ILoggingEvent eventObject) {
+    LogRecord record = Converters.convert(eventObject);
+    synchronized (this) {
+      try {
+        ensurePartition(record.getTs());
+      } catch (IOException | InterruptedException | RuntimeException ex) {
+        this.addError("Unable to setup log file", ex);
+      }
+      try {
+        this.writer.append(record);
+      } catch (IOException | RuntimeException ex) {
+        this.addError("Unable to write log " + record, ex);
+      }
+    }
+  }
+
+  @Override
+  public String toString() {
+    return "AvroDataFileAppender{" + "fileNameBase=" + fileNameBase
+            + ", writer=" + writer + ", fileDate=" + fileDate + ", currentFile="
+            + currentFile + ", destinationPath=" + destinationPath + ", zoneId=" + zoneId + '}';
+  }
+
+
+
+
+}
---
+++ src/test/java/org/spf4j/log/AvroDataFileAppenderTest.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2019 SPF4J.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.spf4j.log;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.IThrowableProxy;
+import ch.qos.logback.classic.spi.LoggerContextVO;
+import java.io.File;
+import java.io.IOException;
+import java.time.ZoneId;
+import java.util.Collections;
+import java.util.Map;
+import org.junit.Assert;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.FileReader;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.spf4j.base.Arrays;
+import org.spf4j.base.avro.LogRecord;
+
+/**
+ * @author Zoltan Farkas
+ */
+public class AvroDataFileAppenderTest {
+
+  private static final Logger LOG = LoggerFactory.getLogger(AvroDataFileAppenderTest.class);
+
+  @Test
+  public void testAvroDataFileAppender() throws IOException {
+    AvroDataFileAppender appender = new AvroDataFileAppender();
+    appender.setDestinationPath(new File(org.spf4j.base.Runtime.TMP_FOLDER));
+    appender.setFileNameBase("testAvroLog");
+    appender.setPartitionZoneID(ZoneId.systemDefault().getId());
+    appender.start();
+    File theFile = appender.getCurrentFile();
+    appender.append(new TestLogEvent());
+    appender.append(new TestLogEvent());
+    appender.stop();
+    FileReader<LogRecord> reader = DataFileReader.openReader(theFile, new SpecificDatumReader<>(LogRecord.class));
+    int i = 0;
+    for (LogRecord rec : reader) {
+      LOG.debug("retrieved", rec);
+      i++;
+    }
+    Assert.assertEquals(2, i);
+
+  }
+
+  private static class TestLogEvent implements ILoggingEvent {
+
+    public TestLogEvent() {
+    }
+
+    @Override
+    public String getThreadName() {
+      return "test";
+    }
+
+    @Override
+    public Level getLevel() {
+      return Level.INFO;
+    }
+
+    @Override
+    public String getMessage() {
+      return "message";
+    }
+
+    @Override
+    public Object[] getArgumentArray() {
+      return Arrays.EMPTY_OBJ_ARRAY;
+    }
+
+    @Override
+    public String getFormattedMessage() {
+      return "message";
+    }
+
+    @Override
+    public String getLoggerName() {
+      return "logger";
+    }
+
+    @Override
+    public LoggerContextVO getLoggerContextVO() {
+      throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public IThrowableProxy getThrowableProxy() {
+      return null;
+    }
+
+    @Override
+    public StackTraceElement[] getCallerData() {
+      throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean hasCallerData() {
+      return false;
+    }
+
+    @Override
+    public Marker getMarker() {
+      return null;
+    }
+
+    @Override
+    public Map<String, String> getMDCPropertyMap() {
+      return Collections.EMPTY_MAP;
+    }
+
+    @Override
+    public Map<String, String> getMdc() {
+      return Collections.EMPTY_MAP;
+    }
+
+    @Override
+    public long getTimeStamp() {
+      return System.currentTimeMillis();
+    }
+
+    @Override
+    public void prepareForDeferredProcessing() {
+      // do notyhing;
+    }
+  }
+
+}
