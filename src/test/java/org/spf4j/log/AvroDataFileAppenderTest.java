0e01f7f30bf8a8dc985760e87d831d9f1d6b0672
Zoltan Farkas <zolyfarkas@yahoo.com>
2019-10-24 21:28:05 -0400

    [cleanup]

--- src/test/java/org/spf4j/log/AvroDataFileAppenderTest.java
+++ src/test/java/org/spf4j/log/AvroDataFileAppenderTest.java
@@ -28,31 +28,9 @@ import java.nio.file.Paths;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
-import org.apache.drill.common.config.DrillConfig;
-import org.apache.drill.common.config.DrillProperties;
-import org.apache.drill.common.exceptions.ExecutionSetupException;
-import org.apache.drill.common.logical.FormatPluginConfig;
-import org.apache.drill.exec.ExecConstants;
-import org.apache.drill.exec.client.DrillClient;
-import org.apache.drill.exec.proto.UserProtos;
-import org.apache.drill.exec.rpc.DrillRpcFuture;
-import org.apache.drill.exec.rpc.user.QueryDataBatch;
-import org.apache.drill.exec.server.Drillbit;
-import org.apache.drill.exec.server.RemoteServiceSet;
-import org.apache.drill.exec.store.SchemaFactory;
-import org.apache.drill.exec.store.StoragePluginRegistry;
-import org.apache.drill.exec.store.avro.AvroFormatConfig;
-import org.apache.drill.exec.store.dfs.FileSystemConfig;
-import org.apache.drill.exec.store.dfs.FileSystemPlugin;
-import org.apache.drill.exec.store.dfs.WorkspaceConfig;
-import org.apache.drill.exec.store.sys.store.provider.ZookeeperPersistentStoreProvider;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -251,111 +229,6 @@ public class AvroDataFileAppenderTest {
     }
     submit.cancel(true);
     appender.stop();
-  }
-
-  /**
-   * see
-   */
-  public static void configureFormatPlugins(final StoragePluginRegistry pluginRegistry,
-          final String storagePlugin, final String avroPath)
-          throws ExecutionSetupException {
-    FileSystemPlugin fileSystemPlugin = (FileSystemPlugin) pluginRegistry.getPlugin(storagePlugin);
-    FileSystemConfig fileSystemConfig = (FileSystemConfig) fileSystemPlugin.getConfig();
-
-    Map<String, FormatPluginConfig> newFormats = new HashMap<>();
-    Optional.ofNullable(fileSystemConfig.getFormats())
-            .ifPresent(newFormats::putAll);
-
-    AvroFormatConfig avroConfig = new AvroFormatConfig();
-    newFormats.put("avro", avroConfig);
-
-    Map<String, WorkspaceConfig> workspaces = new HashMap<>();
-    workspaces.putAll(fileSystemConfig.getWorkspaces());
-    workspaces.put("tmp", new WorkspaceConfig(avroPath, true, "avro", false));
-    workspaces.put("avro", new WorkspaceConfig(avroPath, true, "avro", false));
-    workspaces.put(SchemaFactory.DEFAULT_WS_NAME, new WorkspaceConfig(avroPath, true, "avro", false));
-
-    FileSystemConfig newFileSystemConfig = new FileSystemConfig(
-            fileSystemConfig.getConnection(),
-            fileSystemConfig.getConfig(),
-            workspaces,
-            newFormats);
-
-    newFileSystemConfig.setEnabled(Boolean.TRUE);
-    pluginRegistry.createOrUpdate(storagePlugin, newFileSystemConfig, true);
-  }
-
-  @Test
-  @Ignore
-  // experiments....
-  public void testdrill() throws IOException, InterruptedException, Exception {
-    final AvroDataFileAppender appender = new AvroDataFileAppender();
-    String destFolder = new File(org.spf4j.base.Runtime.TMP_FOLDER + "/avro").getCanonicalPath();
-    appender.setDestinationPath(destFolder);
-    appender.setFileNameBase("tesAvroLog");
-    appender.setPartitionZoneID(ZoneId.systemDefault().getId());
-    appender.start();
-    for (int i = 0; i < 1000; i++) {
-      appender.append(new TestLogEvent(Instant.now(), "", "a", 3, 4, LogAttribute.traceId("cucu")));
-      Thread.sleep(1);
-    }
-    appender.stop();
-    String tmpFolder = org.spf4j.base.Runtime.TMP_FOLDER;
-    Properties props = new Properties();
-    // Properties here mimic those in drill-root/pom.xml, Surefire plugin
-    // configuration. They allow tests to run successfully in IDE.
-    props.setProperty(ExecConstants.SYS_STORE_PROVIDER_LOCAL_ENABLE_WRITE, "false");
-
-    // The CTTAS function requires that the default temporary workspace be
-    // writable. By default, the default temporary workspace points to
-    // dfs.tmp. But, the test setup marks dfs.tmp as read-only. To work
-    // around this, tests are supposed to use dfs. So, we need to
-    // set the default temporary workspace to dfs.tmp.
-    props.setProperty(ExecConstants.DEFAULT_TEMPORARY_WORKSPACE, "dfs.tmp");
-    props.setProperty(ExecConstants.HTTP_ENABLE, "false");
-    props.setProperty("drill.catastrophic_to_standard_out", "true");
-
-    // Verbose errors.
-    props.setProperty(ExecConstants.ENABLE_VERBOSE_ERRORS_KEY, "true");
-
-    // See Drillbit.close. The Drillbit normally waits a specified amount
-    // of time for ZK registration to drop. But, embedded Drillbits normally
-    // don't use ZK, so no need to wait.
-    props.setProperty(ExecConstants.ZK_REFRESH, "0");
-
-    // This is just a test, no need to be heavy-duty on threads.
-    // This is the number of server and client RPC threads. The
-    // production default is DEFAULT_SERVER_RPC_THREADS.
-    props.setProperty(ExecConstants.BIT_SERVER_RPC_THREADS, "2");
-
-    // No need for many scanners except when explicitly testing that
-    // behavior. Production default is DEFAULT_SCAN_THREADS
-    props.setProperty(ExecConstants.SCAN_THREADPOOL_SIZE, "4");
-
-    // Define a useful root location for the ZK persistent
-    // storage. Profiles will go here when running in distributed
-    // mode.
-    props.setProperty(ZookeeperPersistentStoreProvider.DRILL_EXEC_SYS_STORE_PROVIDER_ZK_BLOBROOT,
-            tmpFolder + "/drill/sstore/zk");
-    props.setProperty(ExecConstants.DRILL_TMP_DIR, tmpFolder + "/drill");
-    props.setProperty(ExecConstants.SYS_STORE_PROVIDER_LOCAL_PATH, tmpFolder + "/drill/sstore");
-
-    DrillConfig cfg = DrillConfig.create(props);
-
-    Drillbit bit = new Drillbit(cfg, RemoteServiceSet.getLocalServiceSet());
-    bit.run();
-    final StoragePluginRegistry pluginRegistry = bit.getContext().getStorage();
-
-    configureFormatPlugins(pluginRegistry, "dfs", destFolder);
-
-    DrillClient client = new DrillClient(true);
-    Properties clProps = new Properties();
-    clProps.setProperty(DrillProperties.DRILLBIT_CONNECTION, String.format("localhost:%s", bit.getUserPort()));
-    client.connect(clProps);
-    DrillRpcFuture<UserProtos.CreatePreparedStatementResp> pps
-            = client.createPreparedStatement("select * from dfs.avro.`/`");
-    List<QueryDataBatch> qdb = client.executePreparedStatement(pps.get().getPreparedStatement().getServerHandle());
-    LOG.debug("get result: {}", qdb);
   }
 
 }
